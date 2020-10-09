f447e93b3b282c3af8c595f84612d1fa632805ec
CoJaiKi <cjai.kim@gmail.com>
2020-10-08 22:26:41 -0700

    changed data set to correct form; need to batch and train

--- build_train_test.py
+++ build_train_test.py
@@ -11,22 +11,24 @@ tensor_path = 'tensors/' #path to tensors
 
 def load_data(tensor_path):
     data = [] #list of data chunks
-
+    dataset = None #to be filled with chunks of songs
     for f in os.listdir(tensor_path):
         if 'rate' in f:
             continue
         with open(tensor_path+f,'rb') as p_file:
             song = pickle.load(p_file)
             song_dataset = tf.data.Dataset.from_tensor_slices(song)
-            print(song_dataset.cardinality())
-            #ta = tf.TensorArray(tf.float32,size=0,dynamic_size=True)
-
+            sequences = song_dataset.batch(seq_len, drop_remainder=True)
+            data.append(sequences)
             #lengths = calculate_chunk_size(song.get_shape()[0],seq_len)
-            #ta = ta.split(value=song,lengths=lengths)
             #for i in range(ta.size()):
             #    data.append(ta.read(i))
-    dataset = tf.data.Dataset.from_tensor_slices(data)
 
+    dataset = data[0]
+    for i in range(1,len(data)):
+        dataset = dataset.concatenate(data[i])
+
+    dataset = dataset.map(split_input_target)
     return dataset
 
 #helper method for TensorArray.Split(); requires 1-d array of lengths to be sum of total tensor length
@@ -43,14 +45,10 @@ def calculate_chunk_size(tensor_len,chunk_len):
     return lengths
 
 # Offesets chunks by 1; uses (0 to n-1) as input and (1 to n) as target
-def split_input_target(data):
+def split_input_target(chunk):
 
-    x = []
-    y = []
-
-    for chunk in data:
-        x.append(chunk[:-1])
-        y.append(chunk[1:])
+    x = chunk[:-1]
+    y = chunk[1:]
 
     return x,y
 
@@ -66,8 +64,8 @@ def split_train_test(x,y):
 if __name__ == '__main__':
     data = load_data(tensor_path)
     print(data.cardinality())
-    x,y = split_input_target(data)
-    x_train,x_test,y_train,y_test = split_train_test(x,y)
+    #x,y = split_input_target(data)
+    #x_train,x_test,y_train,y_test = split_train_test(x,y)
 
-    with open('data_file.p','wb') as f:
-        pickle.dump([x_train,x_test,y_train,y_test],f)
+    #with open('data_file.p','wb') as f:
+     #   pickle.dump([x_train,x_test,y_train,y_test],f)
